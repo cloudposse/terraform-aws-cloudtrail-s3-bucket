653ebe48e099352232f88dba4367193f79d568a5
aknysh <andriy.knysh@gmail.com>
2018-03-24 22:02:58 -0400

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "bucket_domain_name" {
 }
 
 output "bucket_id" {
-  value = "${module.s3_bucket.id}"
+  value = "${module.s3_bucket.bucket_id}"
 }
 
 output "bucket_arn" {
-  value = "${module.s3_bucket.arn}"
+  value = "${module.s3_bucket.bucket_arn}"
 }
