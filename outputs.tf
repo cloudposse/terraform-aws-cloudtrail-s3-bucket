96fd68425a5ccdd906716ac72336fc28ac9bc265
aknysh <andriy.knysh@gmail.com>
2018-03-24 22:01:02 -0400

    Initial implementation

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "bucket_domain_name" {
+  value = "${module.s3_bucket.bucket_domain_name}"
+}
+
+output "bucket_id" {
+  value = "${module.s3_bucket.id}"
+}
+
+output "bucket_arn" {
+  value = "${module.s3_bucket.arn}"
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "namespace" {
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+  type        = "string"
+}
+
+variable "stage" {
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  type        = "string"
+}
+
+variable "name" {
+  description = "Name  (e.g. `app` or `cluster`)"
+  type        = "string"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `logs`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
+}
+
+variable "acl" {
+  type        = "string"
+  description = "Canned ACL to apply to the S3 bucket"
+  default     = "log-delivery-write"
+}
+
+variable "region" {
+  type        = "string"
+  default     = "us-east-1"
+  description = "AWS Region for S3 bucket"
+}
