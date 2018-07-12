d5e4826f233433c142be81d61ce6d3925aadb278
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-12 16:09:58 +0300

    Migrate readme yaml

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "bucket_domain_name" {
-  value = "${module.s3_bucket.bucket_domain_name}"
+  value       = "${module.s3_bucket.bucket_domain_name}"
+  description = "FQDN of bucket"
 }
 
 output "bucket_id" {
-  value = "${module.s3_bucket.bucket_id}"
+  value       = "${module.s3_bucket.bucket_id}"
+  description = "Bucket ID"
 }
 
 output "bucket_arn" {
-  value = "${module.s3_bucket.bucket_arn}"
+  value       = "${module.s3_bucket.bucket_arn}"
+  description = "Bucket ARN"
 }
