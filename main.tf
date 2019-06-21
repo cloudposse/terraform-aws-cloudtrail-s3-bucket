69c22f88b1554474a18889d2a0df0fa585d1d783
rverma-nikiai <rohit.verma@niki.ai>
2019-06-21 19:31:30 +0530

    moving back pointer to cloudposse

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/rverma-nikiai/terraform-null-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_bucket" {
-  source                             = "git::https://github.com/rverma-nikiai/terraform-aws-s3-log-storage.git?ref=master"
+  source                             = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=master"
   namespace                          = var.namespace
   stage                              = var.stage
   name                               = var.name
