b34f528939f4c70bbc9af798e4f736a02aa40620
Igor Rodionov <goruha@gmail.com>
2019-08-13 15:28:45 +0600

    Get logging with s3 bucket

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_bucket" {
-  source                             = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.4.1"
+  source                             = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=0.11/logging"
   namespace                          = "${var.namespace}"
   stage                              = "${var.stage}"
   name                               = "${var.name}"
