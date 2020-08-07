94cc9e8b7937f2b0eff6d4a0cbe6d2b2ce6a60f7
Tyler Walker <tylerw@ignw.io>
2020-08-07 11:14:33 -0700

    added internal reference

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source      = "git::https://github.com/brokorus/terraform-null-label.git?ref=master"
   enabled     = var.enabled
   namespace   = var.namespace
   stage       = var.stage
