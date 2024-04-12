4905c5c8287d3156213f57e676083f5665c50c29
X-Guardian <simonheather99@gmail.com>
2024-04-12 17:05:25 +0100

    refine tests

--- examples/complete/fixtures.us-east-2.tfvars
+++ examples/complete/fixtures.us-east-2.tfvars
@@ -5,13 +5,3 @@ namespace = "eg"
 stage = "test"
 
 name = "cloudtrail-s3-bucket"
-
-enable_log_file_validation = true
-
-is_multi_region_trail = false
-
-include_global_service_events = false
-
-enable_logging = true
-
-is_organization_trail = false
--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -15,12 +15,8 @@ module "cloudtrail" {
   source  = "cloudposse/cloudtrail/aws"
   version = "0.23.0"
 
-  enable_logging                = var.enable_logging
-  enable_log_file_validation    = var.enable_log_file_validation
-  include_global_service_events = var.include_global_service_events
-  is_multi_region_trail         = var.is_multi_region_trail
-  is_organization_trail         = var.is_organization_trail
-  s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_id
+  is_multi_region_trail = false
+  s3_bucket_name        = module.cloudtrail_s3_bucket.bucket_id
 
   context = module.this.context
 }
