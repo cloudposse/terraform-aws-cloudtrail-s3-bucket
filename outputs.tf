23178f34820ccdf50ac1873b02765fa5945c8c97
Nuru <Nuru@users.noreply.github.com>
2024-04-15 11:17:41 -0700

    Provide explanation of use of undocumented feature

--- outputs.tf
+++ outputs.tf
@@ -6,8 +6,19 @@ output "bucket_domain_name" {
 output "bucket_id" {
   value       = module.s3_bucket.bucket_id
   description = "Bucket ID"
+  #
+  # Ensure the bucket is fully configured before allowing any use of `bucket_id`.
+  #
+  # Although undocumented, `depends_on` is allowed in an output block.
+  # The `bucket_id` is available before the bucket is fully configured 
+  # with policies, versioning, lifecycle, etc. However, all that 
+  # needs to happen before the bucket can be used as a destination for 
+  # a CloudTrail. While the documented way to ensure this would be
+  # for the user of this module to add a `depends_on` that depends
+  # on this module, since this is such a common need, and since
+  # we can depend on a submodule here rather than this entire module,
+  # we add the `depends_on` block here.
   depends_on = [
-    # The S3 bucket policy resource must be created before the bucket is referenced by CloudTrail
     module.s3_bucket
   ]
 }
